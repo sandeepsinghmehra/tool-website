b63280db8007a484b00611b453d3ea88266dbff6
sandeepsinghmehra <sandysmart017@gmail.com>
2024-06-17 17:05:43 +0530

    Give a seo title, description & keywords

--- src/app/image-optimize/page.tsx
+++ src/app/image-optimize/page.tsx
@@ -1,5 +1,11 @@
 import OptimizeCom from "@/components/Image/Optimization/ImageOpz";
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "Image Optimizer | Resizer",
+  description: `Convert Master Free online Image Resizer. Upload your valid Image file and click to Optimise button. Then you can download your image.`,
+  keywords: ["Image Optimizer", "Online Free Resizer", `Free Image Resizer`,],
+};
 
 function ImageOptimize() {
   
