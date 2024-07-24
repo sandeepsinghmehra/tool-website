659478194194b4d20ca956f4af3d387b986b92e1
sandeepsinghmehra <sandysmart017@gmail.com>
2024-07-24 17:54:26 +0530

    Update some seo change

--- src/app/image-resizer/page.tsx
+++ src/app/image-resizer/page.tsx
@@ -2,9 +2,9 @@ import ImageResizer from "@/components/Image/Optimization/ImageResizer";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Resize image upto 500kb Free Online",
-  description: `Resize your images to specific kilobyte sizes with our free online image resizer tool. Easy and fast image resizing for web and mobile. Use our free online image resizer tool to quickly and easily reduce your image size to a specific number of kilobytes (KB). Perfect for uploading to websites, sharing on social media, or emailing.`,
-  keywords: ["Resize image to 20kb free", "Resize image to 30kb", "resize image to kilobytes", "Resize image upto 500kb",],
+  title: "Resize image under 500kb Free Online",
+  description: `Resize your images to specific kb sizes with our free online image resizer tool. Easy and fast image resizing for web and mobile. Use our free online image resizer tool to quickly and easily reduce your image size to a specific number of kilobytes (KB). Perfect for uploading to websites, sharing on social media, or emailing.`,
+  keywords: ["Resize image to 20kb free", "Resize image to 30kb", "resize image to kb", "Resize image to 100kb",],
 };
 
 function ImageResizerPage() {
