d3599bd6e24b53c2b143b4961373b0819d66f938
sandeepsinghmehra <sandysmart017@gmail.com>
2024-12-02 19:32:49 +0530

    Update keywords

--- src/app/password-generator/page.tsx
+++ src/app/password-generator/page.tsx
@@ -6,7 +6,7 @@ import PasswordGenerator from "./components/PasswordGenerator";
 export const metadata: Metadata = {
   title: "Random Password Generator | Password Generator",
   description: " Generate strong, secure passwords effortlessly with our customizable password generator tool. Enhance your online security with ease.",
-  keywords: ['random password generator', 'strong password generator', 'password generator', 'create strong password', 'free password generator', 'customizable password', "free online random password generator", "online random password generator free", "online random password generator"]
+  keywords: ['free password generator free', 'online password generator free', 'password generator free online', 'free password maker', 'google password maker', 'strong password generator free','random password generator free', 'strong password generator', 'password generator', 'free password generator', 'customizable password', "free online random password generator", "online random password generator free", "online random password generator"]
 };
 
 
